9305850425e0998f82ab2dead4ce5a39d651cd41
Christos Vasilakis <cvasilak@gmail.com>
2015-01-21 15:17:34 +0200

    initial commit

---
+++ FooBar/Foo.swift
@@ -0,0 +1,15 @@
+//
+//  Foo.swift
+//  FooBar
+//
+//  Created by Christos Vasilakis on 1/21/15.
+//  Copyright (c) 2015 Christos Vasilakis. All rights reserved.
+//
+
+import Foundation
+
+public class Foo {
+    public func hello() {
+        println("hello")
+    }
+}
---
+++ FooBar/FooBar.h
@@ -0,0 +1,19 @@
+//
+//  FooBar.h
+//  FooBar
+//
+//  Created by Christos Vasilakis on 1/21/15.
+//  Copyright (c) 2015 Christos Vasilakis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FooBar.
+FOUNDATION_EXPORT double FooBarVersionNumber;
+
+//! Project version string for FooBar.
+FOUNDATION_EXPORT const unsigned char FooBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FooBar/PublicHeader.h>
+
+
